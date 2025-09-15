53b405ddd32b1689153b2878b081d4cf9c2bb854
Ryan G <exogenesis@protonmail.com>
2025-09-15 12:48:55 +1000

    Revert "Implement PWA"
    This reverts commit 6769d63b40065e15e43beb2431833007a0c3ac54.

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,15 +2,3 @@
 import { mount, StartClient } from "@solidjs/start/client";
 
 mount(() => <StartClient />, document.getElementById("app")!);
-
-if ('serviceWorker' in navigator) {
- window.addEventListener('load', () => {
-   navigator.serviceWorker.register('/sw.js')
-     .then((registration) => {
-       console.log('SW registered: ', registration);
-     })
-     .catch((registrationError) => {
-       console.log('SW registration failed: ', registrationError);
-     });
- });
-}
