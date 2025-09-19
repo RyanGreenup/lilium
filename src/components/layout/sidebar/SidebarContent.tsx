17dc352d7a82f230e844818a49e1646ae9edeebc
Ryan G <exogenesis@protonmail.com>
2025-09-19 13:32:54 +1000

    Use url parameters for sidebar content

--- src/components/layout/sidebar/SidebarContent.tsx
+++ src/components/layout/sidebar/SidebarContent.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, Show } from "solid-js";
+import { useSearchParams } from "@solidjs/router";
 import {
   ArrowLeft,
   ArrowRight,
@@ -7,6 +7,7 @@ import {
   Search,
   Sparkles,
 } from "lucide-solid";
+import { createSignal, For, onMount, Show } from "solid-js";
 import { Tabs } from "~/solid-daisy-components/components/Tabs";
 import BacklinksTab from "./tabs/BacklinksTab";
 import DiscussionTab from "./tabs/DiscussionTab";
@@ -17,6 +18,7 @@ import { SidebarSearchContent } from "./tabs/SearchTab";
 
 export const SidebarTabs = () => {
   const [activeTab, setActiveTab] = createSignal(0);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const tabs = [
     { id: 0, label: "Notes", icon: <Notebook class="w-4 h-4" /> },
@@ -26,6 +28,33 @@ export const SidebarTabs = () => {
     { id: 4, label: "Related", icon: <Sparkles class="w-4 h-4" /> },
     { id: 5, label: "Discussion", icon: <MessageSquare class="w-4 h-4" /> },
   ];
+
+  onMount(() => {
+    if (searchParams.sidebar) {
+      switch (searchParams.sidebar) {
+        case "notes":
+          setActiveTab(0);
+          break;
+        case "search":
+          setActiveTab(1);
+          break;
+        case "backlinks":
+          setActiveTab(2);
+          break;
+        case "forward":
+          setActiveTab(3);
+          break;
+        case "related":
+          setActiveTab(4);
+          break;
+        case "discussion":
+          setActiveTab(5);
+          break;
+        default:
+          setActiveTab(0);
+      }
+    }
+  });
 
   return (
     <>
